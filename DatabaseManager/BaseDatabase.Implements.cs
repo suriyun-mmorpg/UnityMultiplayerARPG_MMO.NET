eac73e4d5bc37a019a23598a1e8dfc4f48ab3270
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-27 06:02:01 +0700

    feat: implement database log functions

---
+++ DatabaseManager/BaseDatabase.Implements.cs
@@ -0,0 +1,33 @@
+﻿using LiteNetLibManager;
+
+namespace MultiplayerARPG.MMO
+{
+    public partial class BaseDatabase
+    {
+        public BaseDatabase(IDatabaseUserLogin userLoginManager)
+        {
+            _userLoginManager = userLoginManager;
+            Initialize();
+        }
+
+        public void LogInformation(string tag, string msg)
+        {
+            Logging.Log(tag, msg);
+        }
+
+        public void LogWarning(string tag, string msg)
+        {
+            Logging.LogWarning(tag, msg);
+        }
+
+        public void LogError(string tag, string msg)
+        {
+            Logging.LogError(tag, msg);
+        }
+
+        public void LogException(string tag, Exception ex)
+        {
+            Logging.LogException(tag, ex);
+        }
+    }
+}
