be76a7b2bfc97c7fa7ce0c717f89a033afbb1da1
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-28 04:42:39 +0700

    feat: implement central server data manager

---
+++ CentralServer/CentralServerDataManager.cs
@@ -0,0 +1,25 @@
+﻿namespace MultiplayerARPG.MMO
+{
+    public class CentralServerDataManager : ICentralServerDataManager
+    {
+        public string GenerateCharacterId()
+        {
+            return GenericUtils.GetUniqueId();
+        }
+
+        public string GenerateMapSpawnRequestId()
+        {
+            return GenericUtils.GetUniqueId();
+        }
+
+        public bool CanCreateCharacter(int dataId, int entityId, int factionId)
+        {
+            return DataManager.CharacterCreationData.CanCreateCharacter(dataId, entityId, factionId);
+        }
+
+        public void SetNewPlayerCharacterData(PlayerCharacterData playerCharacterData, string characterName, int dataId, int entityId, int factionId)
+        {
+            DataManager.CharacterCreationData.SetCreateCharacterData(playerCharacterData, playerCharacterData.Id, playerCharacterData.UserId, characterName, dataId, entityId, factionId);
+        }
+    }
+}
