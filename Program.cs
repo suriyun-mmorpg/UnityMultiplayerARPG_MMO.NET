7c1e1d7966d6e891545214a3a1b031a422d59878
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-28 04:43:17 +0700

    feat: implement server running codes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cysharp.Threading;
+using LiteNetLibManager;
+using MultiplayerARPG;
+using MultiplayerARPG.MMO;
+using Newtonsoft.Json;
+
+string configFolder = "./Config";
+string configFilePath;
+configFilePath = configFolder + "/items.json";
+if (File.Exists(configFilePath))
+{
+    Logging.Log($"Found config file: {configFilePath}");
+    string dataAsJson = File.ReadAllText(configFilePath);
+    DataManager.Items = JsonConvert.DeserializeObject<Dictionary<int, MinimalItem>>(dataAsJson);
+}
+configFilePath = configFolder + "/characterCreationData.json";
+if (File.Exists(configFilePath))
+{
+    Logging.Log($"Found config file: {configFilePath}");
+    string dataAsJson = File.ReadAllText(configFilePath);
+    DataManager.CharacterCreationData = JsonConvert.DeserializeObject<CharacterCreationData>(dataAsJson);
+}
+
+const int targetFps = 60;
+using var looper = new LogicLooper(targetFps);
+
+CentralNetworkManager centralNetworkManager;
+centralNetworkManager = new CentralNetworkManager();
+centralNetworkManager.DbServiceClient = new RestDatabaseClient();
+centralNetworkManager.DataManager = new CentralServerDataManager();
+centralNetworkManager.StartServer();
+
+MapSpawnNetworkManager mapSpawnNetworkManager;
+mapSpawnNetworkManager = new MapSpawnNetworkManager();
+mapSpawnNetworkManager.StartServer();
+
+// Register a action to the looper and wait for completion.
+await looper.RegisterActionAsync((in LogicLooperActionContext ctx) =>
+{
+    if (centralNetworkManager != null)
+        centralNetworkManager.ProcessUpdate();
+    if (mapSpawnNetworkManager != null)
+        mapSpawnNetworkManager.ProcessUpdate();
+    // Return false to stop the loop
+    return true;
+});
+
+if (centralNetworkManager != null)
+    centralNetworkManager.StopServer();
+
+if (mapSpawnNetworkManager != null)
+    mapSpawnNetworkManager.StopServer();
