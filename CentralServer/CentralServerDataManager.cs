5504908cae9c7219a5b61306c76717dd5b4d812a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-06-27 18:04:42 +0700

    feat: implement custom data

--- CentralServer/CentralServerDataManager.cs
+++ CentralServer/CentralServerDataManager.cs
@@ -12,14 +12,14 @@
             return GenericUtils.GetUniqueId();
         }
 
-        public bool CanCreateCharacter(int dataId, int entityId, int factionId)
+        public bool CanCreateCharacter(int dataId, int entityId, int factionId, IList<CharacterDataBoolean> publicBools, IList<CharacterDataInt32> publicInts, IList<CharacterDataFloat32> publicFloats)
         {
-            return DataManager.CharacterCreationData.CanCreateCharacter(dataId, entityId, factionId);
+            return DataManager.CharacterCreationData.CanCreateCharacter(dataId, entityId, factionId, publicBools, publicInts, publicFloats);
         }
 
-        public void SetNewPlayerCharacterData(PlayerCharacterData playerCharacterData, string characterName, int dataId, int entityId, int factionId)
+        public void SetNewPlayerCharacterData(PlayerCharacterData playerCharacterData, string characterName, int dataId, int entityId, int factionId, IList<CharacterDataBoolean> publicBools, IList<CharacterDataInt32> publicInts, IList<CharacterDataFloat32> publicFloats)
         {
-            DataManager.CharacterCreationData.SetCreateCharacterData(playerCharacterData, playerCharacterData.Id, playerCharacterData.UserId, characterName, dataId, entityId, factionId);
+            DataManager.CharacterCreationData.SetCreateCharacterData(playerCharacterData, playerCharacterData.Id, playerCharacterData.UserId, characterName, dataId, entityId, factionId, publicBools, publicInts, publicFloats);
         }
     }
 }
