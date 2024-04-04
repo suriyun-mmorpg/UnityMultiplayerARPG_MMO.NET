62afa36729db2b109ff7615cac927f4b1202b7d0
insthync <ittipon.bay@gmail.com>
2024-04-04 18:24:58 +0700

    feat: implement postgresql constructor

---
+++ DatabaseManager/PostgreSQLDatabase.Implements.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiplayerARPG.MMO
+{
+    public partial class PostgreSQLDatabase
+    {
+        public PostgreSQLDatabase(IDatabaseUserLogin userLoginManager) : base(userLoginManager)
+        {
+        }
+    }
+}
